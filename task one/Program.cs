4b2fb40a8540c6ce005a0c4b95d3f01453f7794a
jasmin1686 <j.eshak1686@nub.edu.eg>
2025-06-30 18:53:17 +0300

    Update Program.cs

--- task one/Program.cs
+++ task one/Program.cs
@@ -12,12 +12,12 @@
             Console.WriteLine("Price per small room: $25");
             Console.WriteLine("Price per large room: $35");
             Console.WriteLine($"cost={(value1 * 25) + (value2 * 35)}");
-            // int cost = (value1 * 25) + (value2 * 35);
+            //(another solution) int cost = (value1 * 25) + (value2 * 35);
 
             Console.WriteLine($"Tax={(0.06) * ((value1 * 25) + (value2 * 35))}");
-            // double tax = 0.06 * cost;
+            //(another solution) double tax = 0.06 * cost;
             Console.WriteLine($"Totalcost:{((value1 * 25) + (value2 * 35)) + ((0.06) * ((value1 * 25) + (value2 * 35)))}");
-            // Console.WriteLine($"Total cost: {cost + tax}");
+            //(another solution) Console.WriteLine($"Total cost: {cost + tax}");
             Console.WriteLine("This estimate is valid for 30 days");
         }
     }
